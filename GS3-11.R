8307b5dd8932f358fbd9c2c8e011a868d50a6463
D4ZZL3 <39420233+D4ZZL3@users.noreply.github.com>
2022-02-09 11:09:28 -0500

    Add files via upload

---
+++ GS3-11.R
@@ -0,0 +1,11 @@
+# GS3.11
+# Andrew Dai, 
+
+p.a = seq(.40, 1.0, by=.01)
+p.rej = 1 - pbinom(69, 100, p.a)
+p.rej.2 = 1 - pbinom(73, 100, p.a)
+plot(p.a, p.rej, type="l", main="Power Curve")
+points(p.a, p.rej.2, type = "l")
+
+library(pwr)
+pwr.p.test(h = ES.h(0.8,0.6), n = 100, sig.level = 0.05, alternative ="greater")
